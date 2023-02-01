51293b054aa5461a663a976b6337444d0bb5cef4
AndhikaK <andhika.smandading@gmail.com>
2023-02-01 21:22:45 +0700

    test supabase

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const prisma = new PrismaClient;
+
 type Data = {
-  name: string
+  name: string,
+  posts: any,
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const posts = await prisma.post.findMany()
+  res.status(200).json({ name: 'John Doe', posts: posts })
 }
